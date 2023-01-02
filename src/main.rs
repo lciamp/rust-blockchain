244f5dc7e2569e5695fdecc5a2c7ad5c8ccfa84d
lciamp <l.ciamp@yahoo.com>
2023-01-02 14:53:40 -0500

    is_block_valid function

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,16 @@ pub struct App {
     pub blocks: Vec<Block>,
 }
 
+const DIFFICULTY_PREFIX: &str = "00";
+
+fn hash_to_binary_representation(hash: &[u8]) -> String {
+    let mut res: String = String::default();
+    for c in hash {
+        res.push_str(&format!("{:b}", c));
+    }
+    res
+}
+
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Block {
     pub id: u64,
@@ -36,5 +46,35 @@ impl App {
         } else {
             error!("clould not add block - invalid");
         }
+    }
+
+    fn is_block_valid(&self, block: &Block, previous_block: &Block) -> bool {
+        if block.previous_hash != previous_block.hash {
+            warn!("block with id: {} has wrong previous hash", block.id);
+            return false;
+        } else if !hash_to_binary_representation(
+            &hex::decode(&block.hash).except("can decode from hex"),
+        )
+        .starts_with(DIFFICULTY_PREFIX) {
+            warn!("block with id: {} has invalid difficulty", block.id);
+            return false;
+        } else if block.id != previous_block.id + 1 {
+            warn!(
+                "block with id: {} is not the next block after the latest: {}", 
+                block.id, previous_block.id
+            );
+            return false;
+        } else if hex::encode(calculate_hash(
+            block.id,
+            block.timestamp,
+            &block.previous_hash,
+            &block.data,
+            block.nonce,
+        )) != block.hash
+        {
+            warn!("block with id: {} has invalid hash", block.id);
+            return false;
+        }
+        true
     }
 }
